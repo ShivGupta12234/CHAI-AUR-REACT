6aa3efacc45cbab6e06a911fbfa8705f13b5d8ad
ShivGupta12235 <prakashguptashiv911@gmail.com>
2025-07-07 00:35:09 +0530

    Pushing 09themeSwitcher

--- 09themeSwitcher/src/contexts/theme.js
+++ 09themeSwitcher/src/contexts/theme.js
@@ -4,10 +4,10 @@ export const ThemeContext = createContext({
     themeMode: "light",
     darkTheme: () => {},
     lightTheme: () => {},
-});
+})
 
-export const ThemeProvider = ThemeContext.Provider;
+export const ThemeProvider = ThemeContext.Provider
 
-export default function useTheme() {
-    return useContext(ThemeContext);
+export default function useTheme(){
+    return useContext(ThemeContext)
 }
